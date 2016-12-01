5f082e17297bdc4ebede7e575b94bdec4a467da6
bvellek <bvellek@uw.edu>
2016-11-30 21:49:26 -0800

    change photo url

--- js/main.js
+++ js/main.js
@@ -95,7 +95,7 @@ function displayResult(item, office) {
   //Photo display
   if (item.photoUrl) {
     var photoUrl = item.photoUrl;
-    newResult.find('.headshot-container').css('background-image', 'url(' + photoUrl + ')');
+    newResult.find('.headshot-container').css('background-image', 'url("https:' + photoUrl.substring(5) + '")');
   } else {
     newResult.find('.headshot-container').css('background-image', 'url(img/noIMG.jpg)');
   }
